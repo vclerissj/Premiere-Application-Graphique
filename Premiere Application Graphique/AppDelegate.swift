101a0a699a2a7f131d218f8843e19ef158360322
Vincent Clerissj <vincent.clerissj@viacesi.fr>
2016-04-05 10:33:55 +0200

    Initial Commit

---
+++ Premiere Application Graphique/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Premiere Application Graphique
+//
+//  Created by Vincent Clerissj on 05/04/16.
+//  Copyright © 2016 Vincent Clerissj. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
