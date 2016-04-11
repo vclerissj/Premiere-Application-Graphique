f1afb5ec86aede2cd3e8f15108da20ae3932fa0d
Vincent Clerissj <vincent.clerissj@gmail.com>
2016-04-11 13:43:06 +0200

    add menu

--- Premiere Application Graphique/ViewController.swift
+++ Premiere Application Graphique/ViewController.swift
@@ -9,8 +9,13 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
-    @IBOutlet weak var pullDownButtonClass: NSPopUpButton!
+    
+    var choixPlayerClass1: Class!;
+    var choixPlayerClass2: Class!;
+    
+    @IBOutlet weak var popUpClassJoueur1: NSPopUpButton!
+    @IBOutlet weak var popUpClassJoueur2: NSPopUpButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,8 +29,32 @@ class ViewController: NSViewController {
         }
     }
     
-    @IBAction func PullDownButtonChange(sender: AnyObject) {
+    @IBAction func tabButtonLancerPartie(sender: AnyObject) {
+        switch popUpClassJoueur1.title {
+        case "Guerrier":
+            choixPlayerClass1 = Class.Guerrier;
+        case "Mage":
+            choixPlayerClass1 = Class.Mage;
+        case "Archer":
+            choixPlayerClass1 = Class.Guerrier;
+        default:
+            break;
+        }
         
+        switch popUpClassJoueur2.title {
+        case "Guerrier":
+            choixPlayerClass2 = Class.Guerrier;
+        case "Mage":
+            choixPlayerClass2 = Class.Mage;
+        case "Archer":
+            choixPlayerClass2 = Class.Guerrier;
+        default:
+            break;
+        }
+        
+    }
+    @IBAction func fermerFenetre(sender: AnyObject) {
+        self.dismissController(self);
     }
 }
 
